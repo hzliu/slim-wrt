530e9de3fd3099fcab8b7ad26909064bb9d645cb
Hongzhang Liu <hongzhang.liu@gmail.com>
2022-01-28 19:05:55 +0800

    more customize

--- patches-modules/0002-upnp/custom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# all the custom scripts will run in their own path.
-echo "Script executed from: ${PWD}"
-
-echo "Dummy file."
--- patches-modules/0005-slim-localization-configuration/custom.sh
+++ patches-modules/0005-slim-localization-configuration/custom.sh
@@ -3,5 +3,5 @@
 # all the custom scripts will run in their own path.
 echo "Script executed from: ${PWD}"
 
-# copy fullcone package
+# copy localization package
 cp -rf ${PWD}/slim-localization $SLIM_CFG_WORK_PATH/package
